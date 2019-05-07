bb3cf539a3b60a50cf3182c59fcc5a2c4f5dbd7e
sai-adarsh <sivakumar.sk11865.sk@gmail.com>
2019-05-08 04:57:37 +0530

    new 3
    new 3

---
+++ HackerRank/C++/SaveThePrisoner.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	long long unsigned int i, j, t, x, n, m, s, g=0, d, A;
+	cin>>t;
+	for(i=0; i<t; i++){
+		cin>>n>>m>>s;
+		x = m+s-1;
+		long long unsigned int a[x];
+		for(j=0; j<x; j++){
+			a[j] = j;
+		}
+		for (j=0; j<x; j++){
+			if (a[j]%n==0 && (a[j+1]-1)==a[j]){
+				g++;
+			}
+		}
+		d = (g*n)-x;
+		A = n - d;
+		cout<<"Ans"<<A<<"\n";
+		x = n = m = s = g = d = A = 0;
+	}
+}
